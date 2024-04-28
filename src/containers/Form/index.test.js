2f739cbd0789f39a6a2c70e33eafd194cf155d32
Mathys Henneron <henneron.mathyss@gmail.com>
2024-04-28 16:43:19 +0200

    Correction bug formulaire et test lié

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Form from "./index";
 
 describe("When Events is created", () => {
@@ -10,8 +10,8 @@ describe("When Events is created", () => {
     await screen.findByText("Personel / Entreprise");
   });
 
-  describe("and a click is triggered on the submit button", () => {
-    it("the success action is called", async () => {
+  describe("When Form is created", () => {
+    it("a list of input is displayed", async () => {
       const onSuccess = jest.fn();
       render(<Form onSuccess={onSuccess} />);
       fireEvent(
@@ -22,7 +22,8 @@ describe("When Events is created", () => {
         })
       );
       await screen.findByText("En cours");
-      await screen.findByText("Envoyer");
+      // Utilisation de waitFor pour tester fonction asynchrone
+      await waitFor(() => screen.findByText("Envoyer"), { timeout: 2000 });
       expect(onSuccess).toHaveBeenCalled();
     });
   });
